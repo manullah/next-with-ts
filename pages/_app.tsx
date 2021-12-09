c472e01c27b7afd99c9f2de2e825bfcba61ab912
Imanullah <pengguna21@gmail.com>
2021-12-09 16:21:23 +0700

    add | husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
 
+/**
+ *
+ * @param {object} Component
+ * @param {object} pageProps
+ * @return {object}
+ */
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
